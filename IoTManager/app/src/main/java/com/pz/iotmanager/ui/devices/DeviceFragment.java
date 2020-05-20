12a8cbd2110ac9401c9dbf10fa7d7eb438b3f0dd
Mateusz Gurski <mateuszgurski444@gmail.com>
2020-05-20 04:51:07 +0200

    Wyswietlanie, dodawanie, usuwanie

--- IoTManager/app/src/main/java/com/pz/iotmanager/ui/devices/DeviceFragment.java
+++ IoTManager/app/src/main/java/com/pz/iotmanager/ui/devices/DeviceFragment.java
@@ -71,4 +71,11 @@ public class DeviceFragment extends Fragment
 
     }
 
+    @Override
+    public void onResume() {
+        super.onResume();
+        MainActivity activity = ((MainActivity) getActivity());
+
+        activity.display();
+    }
 }
